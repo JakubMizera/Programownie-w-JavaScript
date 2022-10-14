40c3032b4132ab52cb7e49a4e73b485e10230d20
JakubMizera <kubamiz53@gmail.com>
2022-10-14 12:46:46 +0200

    noob version

--- lab2/anacotokomu/main.js
+++ lab2/anacotokomu/main.js
@@ -1,24 +1,22 @@
-// notatnik z zajęć
-const main = document.querySelector('main')
-const timeoutRef = setTimeout(
-    () => {
-        main.innerHTML = 'From setTimeout'
-    },
-    2000
-)
-let licznik = 0
-const intervalRef = setInterval(
-    () => {
-        main.innerHTML = 'From interval' + licznik++
-    },
-    4000
-)
+let i = 0;
+const images = [];
+const time = 2000;
 
-// kasujemy setInterval
-clearInterval(intervalRef)
+images[0] = 'images/image-1.jpg';
+images[1] = 'images/image-2.jpg';
+images[2] = 'images/image-3.jpg';
+images[3] = 'images/image-4.jpg';
 
-// kasujemy setTimeout
-clearTimeout(intervalRef)
+const changeImg = () => {
+    document.slide.src = images[i];
 
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+    // why is this function is qoutes?
+    setTimeout("changeImg()", time);
+}
 
-// window.requestAnimationFrame
+window.onload = changeImg;
